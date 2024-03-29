63c3f983630878606aad66d30a87aabe40fbd5cd
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-03-30 00:17:27 +0200

    lesson 12

--- src/main/java/com/foretruff/spring/ApplicationRunner.java
+++ src/main/java/com/foretruff/spring/ApplicationRunner.java
@@ -2,10 +2,18 @@ package com.foretruff.spring;
 
 import com.foretruff.spring.database.pool.ConnectionPool;
 import com.foretruff.spring.database.repository.CompanyRepository;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Serializable;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
+        String value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
 //      clazz -> Map<String,Object>
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
