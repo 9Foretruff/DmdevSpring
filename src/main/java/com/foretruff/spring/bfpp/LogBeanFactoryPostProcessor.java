0228378edaf3807a56178c4e40f39cdd75060203
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-03-30 20:33:36 +0200

    lesson 13

---
+++ src/main/java/com/foretruff/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -0,0 +1,19 @@
+package com.foretruff.spring.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
