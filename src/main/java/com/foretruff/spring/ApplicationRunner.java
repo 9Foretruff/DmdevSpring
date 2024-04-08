f46662161642578023ffc3ccdf0d81d8c0f92fff
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-04-08 18:44:42 +0300

    lesson 24

--- src/main/java/com/foretruff/spring/ApplicationRunner.java
+++ src/main/java/com/foretruff/spring/ApplicationRunner.java
@@ -1,9 +1,10 @@
 package com.foretruff.spring;
 
+import com.foretruff.spring.config.ApplicationConfiguration;
 import com.foretruff.spring.database.pool.ConnectionPool;
 import com.foretruff.spring.database.repository.CrudRepository;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.Serializable;
 
@@ -14,7 +15,7 @@ public class ApplicationRunner {
         System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
         System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
 
-        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+        try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
 //      clazz -> Map<String,Object>
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
---
+++ src/main/java/com/foretruff/spring/config/ApplicationConfiguration.java
@@ -0,0 +1,22 @@
+package com.foretruff.spring.config;
+
+import com.foretruff.spring.database.repository.CrudRepository;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.foretruff.spring",
+        useDefaultFilters = false,
+        includeFilters = {
+                @Filter(type = FilterType.ANNOTATION, value = Component.class),
+                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+                @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
+        })
+public class ApplicationConfiguration {
+
+}
