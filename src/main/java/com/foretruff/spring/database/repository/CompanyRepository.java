dd0fc824fa5046c87def6331f09bc491c4866dfa
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-04-08 17:06:28 +0300

    lesson 22

--- src/main/java/com/foretruff/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/foretruff/spring/database/repository/CompanyRepository.java
@@ -7,12 +7,15 @@ import com.foretruff.spring.database.pool.ConnectionPool;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
--- src/main/java/com/foretruff/spring/database/repository/UserRepository.java
+++ src/main/java/com/foretruff/spring/database/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.foretruff.spring.database.repository;
 
 import com.foretruff.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class UserRepository {
     private final ConnectionPool connectionPool;
 
