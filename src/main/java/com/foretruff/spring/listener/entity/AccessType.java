9e52da2aaf4a4dadb88cf33dfd96193cb6be1825
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-04-09 00:40:08 +0300

    lesson 29

---
+++ src/main/java/com/foretruff/spring/listener/entity/AccessType.java
@@ -0,0 +1,5 @@
+package com.foretruff.spring.listener.entity;
+
+public enum AccessType {
+    CREATE, UPDATE, READ, DELETE
+}
---
+++ src/main/java/com/foretruff/spring/listener/entity/EntityEvent.java
@@ -0,0 +1,17 @@
+package com.foretruff.spring.listener.entity;
+
+import org.springframework.context.ApplicationEvent;
+
+public class EntityEvent extends ApplicationEvent {
+    private final AccessType accessType;
+
+    public EntityEvent(Object entity,
+                       AccessType accessType) {
+        super(entity);
+        this.accessType = accessType;
+    }
+
+    public AccessType getAccessType() {
+        return accessType;
+    }
+}
