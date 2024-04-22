a726143332a1a6798422bbf9ef2e0b907e0447b2
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-04-22 18:05:48 +0300

    lesson 43

---
+++ src/test/java/com/foretruff/spring/integration/service/CompanyServiceIT.java
@@ -0,0 +1,33 @@
+package com.foretruff.spring.integration.service;
+
+import com.foretruff.spring.dto.CompanyReadDto;
+import com.foretruff.spring.service.CompanyService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
+//@TestPropertySource -- не подходит для yaml
+@SpringBootTest
+public class CompanyServiceIT {
+
+    private static final Integer COMPANY_ID = 1;
+
+    @Autowired
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+        var actualResult = companyService.findById(COMPANY_ID);
+
+        assertTrue(actualResult.isPresent());
+
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
+    }
+
+}
