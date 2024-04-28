7ab6aa3b85342822e692dd11d530271f093b49f3
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-04-28 21:31:29 +0300

    lesson 54

--- src/main/java/com/foretruff/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/foretruff/spring/database/repository/CompanyRepository.java
@@ -1,12 +1,8 @@
 package com.foretruff.spring.database.repository;
 
 import com.foretruff.spring.database.entity.Company;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
 
-public interface CompanyRepository extends Repository<Company, Integer> {
-    Optional<Company> findById(Integer id);
-
-    void delete(Company entity);
 }
