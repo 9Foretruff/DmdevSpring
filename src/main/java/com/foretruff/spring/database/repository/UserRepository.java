7c356ecb693fb67f24f765a168f20985ae328658
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-04-29 17:03:20 +0300

    lesson 59

--- src/main/java/com/foretruff/spring/database/repository/UserRepository.java
+++ src/main/java/com/foretruff/spring/database/repository/UserRepository.java
@@ -2,11 +2,15 @@ package com.foretruff.spring.database.repository;
 
 import com.foretruff.spring.database.entity.Role;
 import com.foretruff.spring.database.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -24,4 +28,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
            "set u.role = :role " +
            "where u.id in (:ids)")
     int updateRole(Role role, Long... ids);
+
+    Optional<User> findTopByOrderByIdDesc();
+
+    List<User> findTop3ByBirthDateBefore(LocalDate birthdate, Sort sort);
+
+    List<User> findAllBy(Pageable pageable);
+
 }
