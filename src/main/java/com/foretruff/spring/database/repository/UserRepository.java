0ec0328995a788659072d4778bb6c9582d85203f
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-04-29 15:01:48 +0300

    lesson 58

--- src/main/java/com/foretruff/spring/database/repository/UserRepository.java
+++ src/main/java/com/foretruff/spring/database/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.foretruff.spring.database.repository;
 
+import com.foretruff.spring.database.entity.Role;
 import com.foretruff.spring.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -17,4 +19,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
             nativeQuery = true)
     List<User> findAllByUsername(String username);
 
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update User u " +
+           "set u.role = :role " +
+           "where u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
 }
--- src/test/java/com/foretruff/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/foretruff/spring/integration/database/repository/UserRepositoryTest.java
@@ -1,17 +1,35 @@
 package com.foretruff.spring.integration.database.repository;
 
+import com.foretruff.spring.database.entity.Role;
 import com.foretruff.spring.database.repository.UserRepository;
 import com.foretruff.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 @IT
 @RequiredArgsConstructor
 class UserRepositoryTest {
 
     private final UserRepository userRepository;
+
+    @Test
+    void checkUpdate() {
+        var ivan = userRepository.getById(1L);
+        assertSame(Role.ADMIN, ivan.getRole());
+        ivan.setBirthDate(LocalDate.now());
+
+        var resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        assertEquals(2, resultCount);
+
+        var theSameIvan = userRepository.getById(1L);
+        assertSame(Role.USER, theSameIvan.getRole());
+    }
 
     @Test
     void checkQueries() {
