7a870531030ceb6d99535db66715403af035541f
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-05-02 23:52:11 +0300

    lesson 77

--- src/main/java/com/foretruff/spring/http/contoller/GreetingController.java
+++ src/main/java/com/foretruff/spring/http/contoller/GreetingController.java
@@ -1,18 +1,22 @@
 package com.foretruff.spring.http.contoller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("api/v1")
 public class GreetingController {
 
+    @GetMapping("/hello")
     public ModelAndView hello(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
     }
 
+    @GetMapping("/bye")
     public ModelAndView bye(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/bye");
 
