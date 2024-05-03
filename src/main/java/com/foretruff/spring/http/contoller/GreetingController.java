f7fc3501a6c77040c7a019f043d3b387a12a0825
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-05-03 14:43:47 +0300

    lesson 78

--- src/main/java/com/foretruff/spring/http/contoller/GreetingController.java
+++ src/main/java/com/foretruff/spring/http/contoller/GreetingController.java
@@ -1,16 +1,30 @@
 package com.foretruff.spring.http.contoller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("api/v1")
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @CookieValue("JSESSIONID") String jsessionId,
+                              @PathVariable Integer id) {
+        var ageParamValue = request.getParameter("age");
+        var acceptHeader = request.getHeader("accept");
+        var cookies = request.getCookies();
+
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
@@ -22,5 +36,4 @@ public class GreetingController {
 
         return modelAndView;
     }
-
 }
