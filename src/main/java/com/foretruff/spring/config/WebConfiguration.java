05dc4dde42205adcb75673cb39f3137033a5a282
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-05-03 22:45:43 +0300

    lesson 85

---
+++ src/main/java/com/foretruff/spring/config/WebConfiguration.java
@@ -0,0 +1,15 @@
+package com.foretruff.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+//        registry.addConverter(Jsr310Converters.StringToLocalDateTimeConverter.INSTANCE);
+    }
+
+}
