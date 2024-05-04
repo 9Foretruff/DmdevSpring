d92702e15f4c40757728155f4c000ca048ffc616
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-05-04 20:12:13 +0300

    lesson 91

--- src/main/java/com/foretruff/spring/dto/UserCreateEditDto.java
+++ src/main/java/com/foretruff/spring/dto/UserCreateEditDto.java
@@ -1,6 +1,9 @@
 package com.foretruff.spring.dto;
 
 import com.foretruff.spring.database.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Value;
 import lombok.experimental.FieldNameConstants;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -10,11 +13,21 @@ import java.time.LocalDate;
 @Value
 @FieldNameConstants
 public class UserCreateEditDto {
+
+    @Email
     String username;
-//    @DateTimeFormat(pattern = "yyyy-MM-dd")
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     LocalDate birthDate;
+
+    @NotNull
     String firstname;
+
+    @NotNull
+    @Size(min = 3, max = 64)
     String lastname;
+
     Role role;
+
     Integer companyId;
 }
