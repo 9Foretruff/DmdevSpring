3d11234ee584961024df5ecd9c8f1cd2d5dfd9db
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-05-08 21:40:20 +0300

    lesson 106

--- src/main/java/com/foretruff/spring/config/SecurityConfiguration.java
+++ src/main/java/com/foretruff/spring/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.foretruff.spring.config;
 
+import com.foretruff.spring.database.entity.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import static com.foretruff.spring.database.entity.Role.ADMIN;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -18,6 +20,9 @@ public class SecurityConfiguration {
         http
                 .csrf().disable()
                 .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/login", "/users/registration", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/users/\\d+/delete").hasAuthority(ADMIN.getAuthority())
+                        .requestMatchers("/admin/**").hasAuthority(ADMIN.getAuthority())
                         .anyRequest().authenticated()
                 )
                 .logout(logout -> logout
@@ -28,8 +33,7 @@ public class SecurityConfiguration {
 //                .httpBasic(withDefaults())
                 .formLogin(login -> login
                         .loginPage("/login")
-                        .defaultSuccessUrl("/users")
-                        .permitAll())
+                        .defaultSuccessUrl("/users"))
 //                .csrf((csrf) -> csrf
 //                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 //                )
