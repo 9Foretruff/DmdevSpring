ed0de987279bb9eed90bcbb23728bd9046aba62b
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-05-09 22:19:06 +0300

    lesson 112 changes

--- src/main/java/com/foretruff/spring/config/SecurityConfiguration.java
+++ src/main/java/com/foretruff/spring/config/SecurityConfiguration.java
@@ -1,11 +1,15 @@
 package com.foretruff.spring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static com.foretruff.spring.database.entity.Role.ADMIN;
@@ -13,6 +17,12 @@ import static com.foretruff.spring.database.entity.Role.ADMIN;
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfiguration {
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String googleClientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String googleClientSecret;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -46,6 +56,21 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return ClientRegistration.withRegistrationId("google")
+                .clientId(googleClientId)
+                .clientSecret(googleClientSecret)
+                .scope("openid", "profile", "email")
+                .redirectUri("http://localhost:8080/login/oauth2/code/google")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationUri("https://accounts.google.com/o/oauth2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .clientName("Google")
+                .build();
     }
 
 }
