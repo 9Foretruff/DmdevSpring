e951e2d601aa5c0d62376c5d1ad21569eb814ee0
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-05-10 16:04:04 +0300

    lesson 116

---
+++ src/main/java/com/foretruff/spring/http/rest/MessageRestController.java
@@ -0,0 +1,25 @@
+package com.foretruff.spring.http.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/messages")
+public class MessageRestController {
+
+    private final MessageSource messageSource;
+
+    @GetMapping
+    public String getMessage(@RequestParam("key") String key,
+                             @RequestParam("lang") String language) {
+        return messageSource.getMessage(key, null, null, Locale.forLanguageTag(language));
+    }
+
+}
